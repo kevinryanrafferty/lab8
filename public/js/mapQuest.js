e492366ae57938c4dd1ca2a11c91e08668d6acec
Kevin Rafferty <kraffert@ucsd.edu>
2019-02-28 10:31:36 -0800

    Done

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'dq2ck4UrFYh847hPE8ctH4Dn6XJ2uBQ1';
+
+	var map = L.mapquest.map('map', {
+		center: [32.90, -117.228],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
